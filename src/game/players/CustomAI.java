4deda3259eb1a567c668365e42fa5c4749c811e6
vstollen <vstollen@gmail.com>
2019-03-25 22:55:25 +0100

    Add empty CustomAI class

---
+++ src/game/players/CustomAI.java
@@ -0,0 +1,22 @@
+package game.players;
+
+import java.awt.Color;
+
+import game.AI;
+import game.Game;
+
+public class CustomAI extends AI {
+
+	public CustomAI(String name, Color color) {
+		super(name, color);
+	}
+
+	@Override
+	protected void actions(Game game) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}

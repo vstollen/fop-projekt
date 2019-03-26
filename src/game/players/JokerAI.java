45b58e019347910f86f87a34a21906b493a2cfcc
vstollen <vstollen@gmail.com>
2019-03-26 10:21:08 +0100

    Add JavaDocs to Joker AI

--- src/game/players/JokerAI.java
+++ src/game/players/JokerAI.java
@@ -23,19 +23,42 @@ public class JokerAI extends CustomAI {
 		super.actions(game);
 	}
 	
+	/**
+	 * Nutzt intelligent verfügbare Joker
+	 * @param game Das aktuelle Spiel
+	 */
 	private void useJokers(Game game) {
 		Collection<Joker> usableJokers = getUsableJokers();
 		
 		for (Joker joker : usableJokers) {
-			if (joker instanceof TroopbonusJoker) {
-				if (getCastles(game).size() < 3) {
-					joker.invoke();
-					game.logIfPossible(joker.getLogMessage(), this);
-				}
-			}
+			possiblyUseTroopbonus(joker, game);
 		}
 	}
 	
+	/**
+	 * Prüft ob der Joker ein Truppenbonus Joker ist und setzt ihn gegebenenfalls ein,
+	 * falls der Spieler weniger als 3 Burgen hält.
+	 * @param joker Der Joker
+	 * @param game Das aktuelle Spiel
+	 */
+	private void possiblyUseTroopbonus(Joker joker, Game game) {
+		
+		if (!(joker instanceof TroopbonusJoker)) {
+			return;
+		}
+		
+		if (getCastles(game).size() > 2) {
+			return;
+		}
+		
+		joker.invoke();
+		game.logIfPossible(joker.getLogMessage(), this);
+	}
+	
+	/**
+	 * Bildet eine Liste aus allen aktuell verfügbaren Jokern
+	 * @return alle aktuell verfügbaren Joker
+	 */
 	private Collection<Joker> getUsableJokers() {
 		LinkedList<Joker> usableJokers = new LinkedList<>();
 		
